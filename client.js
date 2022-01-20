cbfe7c68748277c754a5c61acc89ce18cb44673f
Hongling Zhang <jolinhlzhang@email.com>
2022-01-20 10:06:12 -0700

    W2D3: export/import module

---
+++ client.js
@@ -0,0 +1,17 @@
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: // IP address here,
+    port: // PORT number here,
+  });
+  
+  conn.on('data', function(message){
+    console.log(`server sent:`,message);
+  });
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,20 +1,6 @@
+const connect = require("./client");
+
 const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: // IP address here,
-    port: // PORT number here,
-  });
-  
-  conn.on('data', function(message){
-    console.log(`server sent:`,message);
-  });
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
 
 console.log("Connecting ...");
 
