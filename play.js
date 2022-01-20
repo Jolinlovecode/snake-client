7b5a5c1c3d09d53c8eeb88d04ea9e807e939f506
Hongling Zhang <jolinhlzhang@email.com>
2022-01-20 09:23:11 -0700

    W2D3:client code setup

---
+++ play.js
@@ -0,0 +1,20 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: // IP address here,
+    port: // PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+console.log("Connecting ...");
+
+client.on('connect', function(){
+  console.log(`i have connected to the server.`);
+});
