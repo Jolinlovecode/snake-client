c19e200c4946449a85e1937cf9be0a59fd8fab6f
Hongling Zhang <jolinhlzhang@email.com>
2022-01-20 11:04:06 -0700

    W2D3

--- play.js
+++ play.js
@@ -4,3 +4,17 @@ const net = require("net");
 
 console.log("Connecting ...");
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  
+
+  stdin.on("data", handleUserInput);
+  const handleUserInput = function (message) {
+    conn.write(message);
+  };
+
+  return stdin;
+};
